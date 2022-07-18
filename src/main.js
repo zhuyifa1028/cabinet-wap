d3b573f9010697e8fd7328bef015a650cd10691a
留行亭长丶 <zhuyifa1028@gmail.com>
2022-07-18 09:00:12 +0800

    京东风格的移动端组件库

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import {createApp} from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App'
+import store from '@/store'
+import router from '@/router'
 
-createApp(App).use(store).use(router).mount('#app')
+import NutUI from '@nutui/nutui'
+import '@nutui/nutui/dist/style.css'
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(NutUI)
+    .mount('#app')
